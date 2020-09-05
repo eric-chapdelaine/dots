9917d0680f92777de2aea082afdcfdc7088ed511
Eric Chapdelaine <emchap4@gmail.com>
2020-09-05 17:09:38 -0400

    Removed calendar function and added multi-display support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 #launch polybar
-polybar example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Polybar launched..."
