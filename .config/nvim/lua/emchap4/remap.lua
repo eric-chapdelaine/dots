8e6c2863be190a82eacc3303d112c51c84f752e3
Eric Chapdelaine <emchap4@gmail.com>
2025-09-02 16:38:47 -0400

    update from mac

--- .config/nvim/lua/emchap4/remap.lua
+++ .config/nvim/lua/emchap4/remap.lua
@@ -1,2 +1,23 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "\\", vim.cmd.Ex)
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- share clipboard with OS and neovim
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Exit terminal mode 
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.o.smartindent = false
+vim.o.autoindent = false
+
